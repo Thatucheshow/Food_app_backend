1a60a33e33f8a4dfed9cd6e36220553898288832
ThatUche <newuser@Uches-MacBook-Air.local>
2022-06-24 10:54:43 -0400

    second main commit

--- foodapp/foodapp_api/models.py
+++ foodapp/foodapp_api/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Food(models.Model):
-
-    
+    title = models.CharField(max_length=32)
+    description = models.CharField()
+    image = models.CharField()
+    price = models.CharField()
+    extraoption = models.CharField()
+    date = models.DateField()
